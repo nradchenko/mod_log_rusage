65ab9094a6e75a3ffdcd6c9b69aeeb0540aadd1b
Nikita Radchenko <nradchenko@protonmail.com>
2023-09-28 13:28:49 +0300

    Add license and copyright notice

--- mod_log_rusage.c
+++ mod_log_rusage.c
@@ -1,7 +1,22 @@
 /*
- * mod_log_rusage.c: resource usage logging
- * Author: Nikita Radchenko <nradchenko@protonmail.com>
- *
+ mod_log_rusage.c: resource usage logging for apache2
+ Copyright (C) 2023 Nikita Radchenko <nradchenko@protonmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
  * This module implements utime and stime logging using %{format}z
  * format string for LogFormat and CustomLog directives.
  *
